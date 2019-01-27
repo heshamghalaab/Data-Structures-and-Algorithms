f92acb170854a37ead6086d9c7d8f50ba32d5331
Hesham <hesham@clueapps.net>
2019-01-27 11:52:16 +0200

    adding linear search algorithm.

--- LinearSearch.playground/Contents.swift
+++ LinearSearch.playground/Contents.swift
@@ -1,1 +1,23 @@
 //// linear search ////
+func search(from numbers: [Int], value: Int){
+    
+    var indexOfTheValue: Int? = nil
+    for (i, number) in numbers.enumerated(){
+        if number == value{
+            indexOfTheValue = i
+        }
+    }
+    
+    // print the index of the last occurence of the value.
+    if let i = indexOfTheValue{
+        print("found at index \(i).")
+    }else {
+        print("not found")
+    }
+}
+
+search(from: [1, 2, 3, 4, 1], value: 1)
+
+/*
+The time complexity of the linear search is O(N) because each element in an array is compared only once.
+*/
